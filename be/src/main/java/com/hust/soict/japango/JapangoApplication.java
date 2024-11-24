8abdc622ae97415c3af6a92dd1a94ba57bb6ea05
Lê Chí Dũng <dung.lc@sis.hust.edu.vn>
2024-11-24 14:09:03 +0700

    init BE

---
+++ be/src/main/java/com/hust/soict/japango/JapangoApplication.java
@@ -0,0 +1,13 @@
+package com.hust.soict.japango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapangoApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/com/hust/soict/japango/JapangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.soict.japango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
